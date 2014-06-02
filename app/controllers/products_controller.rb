417977fcdcb5d63bb0537fd07e0d64c8e7ad5243
Juliana Crispo <julianacrispo@gmail.com>
2014-06-02 15:51:37 -0400

    Added create and update to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,33 @@ class ProductsController < ApplicationController
   end
 
   def new
+  	@product = Product.new
   end
 
+  def create
+  @product = Product.new(params.require(:product).permit(:name))
+	  if @product.save
+	    flash[:notice] = "Product was saved."
+	    redirect_to @product
+	  else
+	    flash[:error] = "There was an error saving the product. Please try again."
+	    render :new
+	  end
+	end
+
   def edit
+  	@product = Product.find(params[:id])
   end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params.require(:product).permit(:name))
+      flash[:notice] = "Product was updated."
+      redirect_to @product
+    else
+      flash[:error] = "There was an error saving the product. Please try again."
+      render :edit
+    end
+  end
+
 end
